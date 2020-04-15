bc18b6588b95c5e2f13c539a9a7988b959295893
Patrick Lee <torkins@gmail.com>
2020-04-14 23:48:51 -0400

    .

--- src/components/Game.js
+++ src/components/Game.js
@@ -12,19 +12,47 @@ import {UserState} from "../logic/userlogic"
     */
 
 class Game extends React.Component {
+    componentDidMount() {
+      fetch("https://fervent-ardinghelli-aa4089.netlify.com/.netlify/functions/create_game")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    gameState: result
+                });
+            },
+            (error) => {
+                console.error("OH SHIT: " + error);
+                this.setState({
+                    isLoaded: false,
+                    error
+                });
+            }
+        );
+    };
+
     render() {
         let userState = new UserState("vanessa"); 
+        const { error, gameState, isLoaded } = this.state; 
+        /*
         let gameState = {
             gameId: "gecko",
             currentQuestioner: "vanessa"
         }
-        
-        return (
-            <div className="game">
-                <PlayerList />
-                <PlayerHand gameState={gameState} userState={userState} />
-            </div>
-        );
+        */
+        if (error) {
+            return <div>Shit dog, there was an error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div className="game">
+                    <PlayerList />
+                    <PlayerHand gameState={gameState} userState={userState} />
+                </div>
+            );
+        }
     }
 }
 
