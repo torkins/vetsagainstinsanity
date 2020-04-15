febb7a692063877a5d08a6775c9004ab48baf86a
Patrick Lee <torkins@gmail.com>
2020-04-15 09:20:07 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { IdentityContextProvider } from 'react-netlify-identity';
 import Game from './components/Game';
+
+const url = "https://fervent-ardinghelli-aa4089.netlify.com/";
 
 
 
 function App() {
   return (
-      <Game />
+      <IdentityContextProvider url={url}>
+          <Game />
+      </IdentityContextProvider>
   );
 }
 
