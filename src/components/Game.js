cf78bd24f21129078aeeba8c1586c42aae4815a9
Patrick Lee <torkins@gmail.com>
2020-04-16 10:00:30 -0400

    .

--- src/components/Game.js
+++ src/components/Game.js
@@ -22,29 +22,6 @@ class Game extends React.Component {
         };
     }
 
-    componentDidMount() {
-      fetch("https://fervent-ardinghelli-aa4089.netlify.com/.netlify/functions/create_game")
-        .then(res => res.json())
-        .then(
-            (result) => {
-                console.info("result");
-                this.setState({
-                    isLoaded: true,
-                    gameState: result.data
-                });
-                console.info("set");
-            },
-            (error) => {
-                console.error("OH SHIT: " + error);
-                this.setState({
-                    isLoaded: false,
-                    error
-                });
-                console.info("set");
-            }
-        );
-    }
-
     render() {
         const { error, userState, gameState, isLoaded } = this.state; 
         console.info("render3");
