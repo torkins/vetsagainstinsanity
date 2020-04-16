6a3d4ea6a9228274fdee76dd4ee6e5ec0fe867a4
Patrick Lee <torkins@gmail.com>
2020-04-16 08:56:42 -0400

    .

--- functions/get_question_deck.js
+++ functions/get_question_deck.js
@@ -5,7 +5,7 @@ var q = faunadb.query;
 exports.handler = function(event, context, callback) {
     var client = new faunadb.Client({ secret: 'fnADpNJRxKACEjNGbUCEnPNjOjbRG12_5qJ7VOEu' });
 
-    var gameType = "vetsagainstinstanity";
+    var gameType = "vetsagainstinsanity";
 
     client.query(
       q.Get(
