35aecb586a5f55033445e2cf14600f4b0c488e0b
Patrick Lee <torkins@gmail.com>
2020-04-16 10:46:21 -0400

    .

--- functions/create_game.js
+++ functions/create_game.js
@@ -12,7 +12,7 @@ exports.handler = function(event, context, callback) {
     client.query(
       q.Create(
         q.Collection('GameState'),
-          { "data": {gameId: "heyguys" } }
+          { "data": data } 
       )
     )
     .then(
