1a064cc93fd9ac1760b5275b7ebbfcfc36b21692
Patrick Lee <torkins@gmail.com>
2020-04-16 10:22:10 -0400

    .

--- functions/create_game.js
+++ functions/create_game.js
@@ -6,7 +6,7 @@ exports.handler = function(event, context, callback) {
     var client = new faunadb.Client({ secret: 'fnADpNJRxKACEjNGbUCEnPNjOjbRG12_5qJ7VOEu' });
 
     var gamestate = event.body; 
-    console.info("gamestate: " + gamestate);
+    console.info("gamestate: " + (typeof gamestate));
 
     client.query(
       q.Create(
