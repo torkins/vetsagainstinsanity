32259e1652a44efd7f18a04a7129041df77efd18
Patrick Lee <torkins@gmail.com>
2020-04-17 11:06:20 -0400

    .

--- functions/get_pending_games.js
+++ functions/get_pending_games.js
@@ -5,18 +5,20 @@ var q = faunadb.query;
 exports.handler = function(event, context, callback) {
     var client = new faunadb.Client({ secret: 'fnADpNJRxKACEjNGbUCEnPNjOjbRG12_5qJ7VOEu' });
 
-    var gameType = "vetsagainstinsanity";
-
+    console.info("higuys");
     client.query(
       q.Get(
         q.Match(q.Index('games_by_hasstarted'), false)
       )
     )
     .then(
-        (ret) => callback(null, {
-            statusCode: 200,
-            body: JSON.stringify(ret)
-        }),
+        (ret) => {
+            console.info("pending: " + JSON.stringify(ret));
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(ret)
+            });
+        },
         (err) => {
             console.info("ERR: " + JSON.stringify(err));
             callback(err)
