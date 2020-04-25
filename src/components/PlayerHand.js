e6aed64c5d19facad6f9529f629fc7f75f1ba073
Patrick Lee <torkins@gmail.com>
2020-04-25 11:50:52 -0400

    .

--- src/components/PlayerHand.js
+++ src/components/PlayerHand.js
@@ -141,13 +141,20 @@ class AnswererHand extends React.Component {
                 )
             ) : undefined;
 
-        let selectedAnswers = this.state.unconfirmedAnswerIds.map(id => {
-            let card = getAnswerCardFromId(gameState, id);
-            let unSelectAnswer = () => this.removeUnconfirmedAnswer(id); 
-            return (
-                <button className="button-primary" onClick={unSelectAnswer}>{card.text}</button>
-            );
-        });
+        let selectedAnswers = (
+            <div>
+            Selected Answers:
+            {
+                this.state.unconfirmedAnswerIds.map(id => {
+                    let card = getAnswerCardFromId(gameState, id);
+                    let unSelectAnswer = () => this.removeUnconfirmedAnswer(id); 
+                    return (
+                        <button className="button-primary" onClick={unSelectAnswer}>{card.text}</button>
+                    );
+                });
+            }
+            </>
+        );
 
         return (
             <>
